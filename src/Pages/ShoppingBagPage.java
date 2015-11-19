e7a87a60f34f0ad25c174b2f74aac3a3c62d8cc8
ydesanti <ydesanti@gmail.com>
2015-11-19 17:11:19 -0600

    Adding more webelements

--- src/Pages/ShoppingBagPage.java
+++ src/Pages/ShoppingBagPage.java
@@ -16,11 +16,26 @@ public class ShoppingBagPage {
 	@FindBy(xpath  = "(//input[@value='Check Out'])[1]")
 	WebElement sbCheckoutButton;
 	
+	@FindBy(xpath  = "(//input[@value='Check out with'])[1]")
+	WebElement sbCheckoutWithPaypalButton;
+	
 	@FindBy(id  = "shipMeRadio")
 	WebElement sbShiptoMeRadioButton;
 	
+	@FindBy(id  = "promoNumber")
+	WebElement sbPromoCodeTextField;
+	
+	@FindBy(xpath  = "//input[@value = 'Redeem']")
+	WebElement sbPromoCodeRedeemButton;
+	
 	@FindBy(id  = "shipOtherRadio")
 	WebElement sbShiptoMultipleAddressesRadioButton;
+	
+	@FindBy(xpath  = "id('un_clearcart')//a")
+	WebElement sbGoToWLLink;
+	
+	@FindBy(xpath  = "id('clearSB')/span")
+	WebElement sbClearSBLink;
 
 	protected WebElement getProductInSB(String productID) {
 		WebElement emailInputField = driver.findElement(By.xpath("//div[@class = 'sbItemAttributes']/div[contains(.,'209410')]/../../.."));
