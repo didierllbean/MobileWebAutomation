cbc22a06dcc40ef561f2f766590a4eb94d13faaa
ydesanti <ydesanti@gmail.com>
2016-01-27 15:42:30 -0600

    ignoring output files

--- src/Tools/Utilities.java
+++ src/Tools/Utilities.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
-import com.relevantcodes.extentreports.LogStatus;
-
 import Pages.ProductPageCore;
 import Test.TestCaseConfiguration;
 import Tools.Constants;
